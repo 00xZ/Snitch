a4d5394b566f3acb683396386229273e11f8c2e4
00xZ <eyezikbra@gmail.com>
2024-02-23 00:58:37 -0600

    Create js_secrest_loop.sh
    It'll scan javascript.txt file for juicy stuff, edit the grep as you wish

---
+++ js_secrest_loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Fetching on: $1"
+cd output/jsdump/
+while read line;do
+    echo $line
+    wget $line 
+done < $1
+grep -r -E "aws_access_key|aws_secret_key|api key|passwd|pwd|heroku|slack|firebase|swagger|aws_secret_key|aws key|password|ftp password|jdbc|db|sql|secret jet|config|admin|pwd|json|gcp|htaccess|ssh key|access key|secret token|oauth_token|oauth_token_secret|smtp" output/jsdump/*.js
+
