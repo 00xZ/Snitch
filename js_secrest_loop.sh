c235fd086536f87e5d89191557d74ba26bacabc3
00xZ <eyezikbra@gmail.com>
2024-02-23 01:00:28 -0600

    Update js_secrest_loop.sh

--- js_secrest_loop.sh
+++ js_secrest_loop.sh
@@ -7,3 +7,4 @@ while read line;do
 done < $1
 grep -r -E "aws_access_key|aws_secret_key|api key|passwd|pwd|heroku|slack|firebase|swagger|aws_secret_key|aws key|password|ftp password|jdbc|db|sql|secret jet|config|admin|pwd|json|gcp|htaccess|ssh key|access key|secret token|oauth_token|oauth_token_secret|smtp" output/jsdump/*.js
 
+### Change wget to curl for less mess you will have to pipe the grep after the curl
